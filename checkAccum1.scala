d1fef1cf6342ea5a0004db6dcc97add0553235dc
Gaji Note <gaji_note@seigaji.info>
2024-05-11 22:40:54 +0900

    class val type inference

--- checkAccum1.scala
+++ checkAccum1.scala
@@ -3,16 +3,9 @@
   class ChecksumAccumulator {
     // class definication
     private var sum = 0
-    
-    def add(b: Byte): Unit = {
-      sum += b
-    }
-
-    def checksum(): Int = {
-      return ~(sum & 0xFF) + 1
-    }
+    def add(b: Byte) sum += b
+    def checksum(): = ~(sum & 0xFF) + 1
   }
-
   val acc = new ChecksumAccumulator
   val csa = new ChecksumAccumulator
 
