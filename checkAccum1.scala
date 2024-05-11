9d37339d81f5d8505ad8900b12147f7b9ac56608
Gaji Note <gaji_note@seigaji.info>
2024-05-11 22:43:35 +0900

    add class type description

--- checkAccum1.scala
+++ checkAccum1.scala
@@ -3,8 +3,8 @@
   class ChecksumAccumulator {
     // class definication
     private var sum = 0
-    def add(b: Byte) sum += b
-    def checksum(): = ~(sum & 0xFF) + 1
+    def add(b: Byte): Unit = { sum += b }
+    def checksum(): Int = ~(sum & 0xFF) + 1
   }
   val acc = new ChecksumAccumulator
   val csa = new ChecksumAccumulator
